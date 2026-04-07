e68a41141351f21e27c43678b77f1f473862732d
Glauco Neto <glauconeto@tutanota.com>
2026-04-07 17:05:19 -0300

    Inicio do projeto

---
+++ tailwindcss.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
